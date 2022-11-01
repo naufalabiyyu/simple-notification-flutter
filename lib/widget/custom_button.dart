d33530adf38492124d13a5e8dd3e22a157ff3512
naufal abiyyu <naufalabiyyu28@gmail.com>
2022-11-01 10:51:55 +0700

    first commit

---
+++ lib/widget/custom_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      buttonColor: Colors.grey[300],
+      minWidth: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          onPrimary: Colors.black87,
+          primary: Colors.grey[300],
+          minimumSize: const Size(88, 36),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(2))),
+        ),
+        onPressed: onPressed,
+        child: Text(text),
+      ),
+    );
+  }
+}
